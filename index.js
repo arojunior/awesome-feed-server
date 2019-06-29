29e103beccc42f1f5069848725837e550daf3472
Junior Oliveira <arojunior@gmail.com>
2019-06-29 19:27:55 -0300

    add travis for build and now for deployment

--- index.js
+++
@@ -1,6 +0,0 @@
-const token = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  return `378a922ba0f54c541a4281e8519020084373d0b1`;
-};
-
-module.exports = token;
--- src/constants.js
+++ src/constants.js
@@ -9,4 +9,4 @@ export const HTTP_STATUS = {
 export const isProduction = process.env.NODE_ENV === `production`;
 export const SERVER_PORT = process.env.PORT || 3001;
 export const GITHUB_GRAPHQL = `https://api.github.com/graphql`;
-export const GITHUB_TOKEN = `378a922ba0f54c541a4281e8519020084373d0b1`;
+export const { GITHUB_TOKEN } = process.env;
