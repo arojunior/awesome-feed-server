bcb2072cbceb05590f4d4b26cfbe87e162dc0c84
Junior Oliveira <arojunior@gmail.com>
2019-12-09 16:23:06 +0100

    adjust profile data

--- src/graphql/resolvers/profileResolver.js
+++ src/graphql/resolvers/profileResolver.js
@@ -2,7 +2,13 @@ import profileQuery from '../queries/profileQuery';
 
 const getProfile = (_, { username }) => {
   return profileQuery(username)
-    .then(({ data: { data } }) => data)
+    .then(({ data: { data } }) => ({
+      avatarUrl: data.user.avatarUrl,
+      login: data.user.login,
+      followers: data.user.followers.totalCount,
+      following: data.user.following.totalCount,
+      starredRepositories: data.user.starredRepositories.totalCount,
+    }))
     .catch(console.log);
 };
 
