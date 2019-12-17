ec0b34a2dd71ccfd9317e6e1c44fb02665bf6bbb
Junior Oliveira <arojunior@gmail.com>
2019-12-17 10:28:00 +0100

    trying to change cursor behavior

--- src/graphql/queries/feedQuery.js
+++ src/graphql/queries/feedQuery.js
@@ -3,7 +3,7 @@ import { GITHUB_GRAPHQL, GITHUB_TOKEN } from '../../constants';
 
 const getTimelineQuery = ({ username, cursor = null }) => `query {
   user(login: "${username}") {
-    following(last: 10, before: ${cursor}) {
+    following(last: 10, after: ${cursor}) {
       edges {
         cursor
       }
